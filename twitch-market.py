10e6e889d7ef3c40861069e6a24cef7f91689c53
Ryan Schaefer <contact.devsquid@gmail.com>
2017-01-24 21:23:51 -0600

    First Commit

--- twitch-market.py
+++ twitch-market.py
@@ -1,1 +1,26 @@
-print "test2"
+"""
+(C) Ryan Schaefer 2017
+A platform to facilitate trades between channel currencies on twitch
+"""
+import websocket
+import re
+from BotPass import PASSWORD
+
+WhisperSocket = websocket.WebSocket()
+WhisperSocket.connect('wss://irc-ws.chat.twitch.tv')
+WhisperSocket.send('PASS %s\r\n' % (PASSWORD))
+WhisperSocket.send('NICK %s\r\n' % ('squid_coin_bot'))
+WhisperSocket.send('JOIN #jtv\r\n')
+WhisperSocket.send('CAP REQ :twitch.tv/commands\r\n')
+
+class WhisperHandler(object):
+    def __init__(self, message):
+        try:
+            ReObj = re.search(r':(\w+)!\w+@.+:(.+)', message)
+            self.user = ReObj.group(0)
+            self.message = ReObj.group(1)
+        except AttributeError:
+            return False
+    def handle(self):
+        pass
+re.search(r':(\w+)!\w+@.+:(.+)', '')
